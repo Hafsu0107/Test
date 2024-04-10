b2bc87ec96ff76b1e41ceb042f4538cc4b0581e2
Hafsu <146491332+Hafsu0107@users.noreply.github.com>
2024-04-11 00:51:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// if/else
+
+// Ans
+let logged = true;
+if (login) {
+  console.log("welcome");
+} else {
+  console.log("please login");
+}
+
+// ans 2
+
+let temperature = 50;
+
+if (temperature > 40) {
+  console.log(
+    "IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave"
+  );
+}
+
+// ans 3
+
+let money = 1500000;
+
+if ((bankAccount = 1000000)) {
+  console.log("Congratulations! You are a millionaire");
+} else {
+  console.log("I'm sorry, you are not a millionaire");
+}
+
+// Ans 4
+let carElectric = false;
+
+if (carElectric) {
+  console.log("Tesla company nai bnaye hai ");
+} else {
+  console.log("Tesla ne nahi banai");
+}
+
+// Ans 5
+let currentScore = 50;
+let oldScore = 30;
+if (currentScore < oldScore) {
+  console.log("Congratulations!! Your new high score is <user's high score>");
+} else {
+  console.log(
+    "Try Again. You just need <the number of more points needed to beat the high score> to win!"
+  );
+}
+
+// Ans 6
